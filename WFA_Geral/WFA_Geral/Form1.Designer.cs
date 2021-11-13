f34ce7f6440104a866801d9b96427c3a584c976d
mmarques-ssz <37246763+mmarques-ssz@users.noreply.github.com>
2021-11-12 21:07:50 -0300

    Add files via upload

---
+++ WFA_Geral/WFA_Geral/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace WFA_Geral
+{
+    partial class frmSoma
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtN1 = new System.Windows.Forms.TextBox();
+            this.btnSomar = new System.Windows.Forms.Button();
+            this.lblMensagem = new System.Windows.Forms.Label();
+            this.txtN2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(356, 286);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(29, 31);
+            this.lblResultado.TabIndex = 0;
+            this.lblResultado.Text = "0";
+            // 
+            // txtN1
+            // 
+            this.txtN1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtN1.Location = new System.Drawing.Point(150, 97);
+            this.txtN1.Name = "txtN1";
+            this.txtN1.Size = new System.Drawing.Size(150, 38);
+            this.txtN1.TabIndex = 1;
+            this.txtN1.MouseLeave += new System.EventHandler(this.txtPipoca_MouseLeave);
+            this.txtN1.MouseHover += new System.EventHandler(this.txtPipoca_MouseHover);
+            // 
+            // btnSomar
+            // 
+            this.btnSomar.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSomar.Location = new System.Drawing.Point(218, 172);
+            this.btnSomar.Name = "btnSomar";
+            this.btnSomar.Size = new System.Drawing.Size(137, 63);
+            this.btnSomar.TabIndex = 2;
+            this.btnSomar.Text = "Somar";
+            this.btnSomar.UseVisualStyleBackColor = true;
+            this.btnSomar.Click += new System.EventHandler(this.btnPipoca_Click);
+            // 
+            // lblMensagem
+            // 
+            this.lblMensagem.AutoSize = true;
+            this.lblMensagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMensagem.Location = new System.Drawing.Point(28, 427);
+            this.lblMensagem.Name = "lblMensagem";
+            this.lblMensagem.Size = new System.Drawing.Size(0, 25);
+            this.lblMensagem.TabIndex = 3;
+            // 
+            // txtN2
+            // 
+            this.txtN2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtN2.Location = new System.Drawing.Point(310, 97);
+            this.txtN2.Name = "txtN2";
+            this.txtN2.Size = new System.Drawing.Size(150, 38);
+            this.txtN2.TabIndex = 4;
+            this.txtN2.MouseLeave += new System.EventHandler(this.txtPipoca_MouseLeave);
+            this.txtN2.MouseHover += new System.EventHandler(this.txtN2_MouseHover);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(150, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 31);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "1º número";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(310, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 31);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "2º número";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(197, 286);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 31);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Resultado:";
+            // 
+            // frmSoma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 482);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtN2);
+            this.Controls.Add(this.lblMensagem);
+            this.Controls.Add(this.btnSomar);
+            this.Controls.Add(this.txtN1);
+            this.Controls.Add(this.lblResultado);
+            this.Name = "frmSoma";
+            this.Text = "Soma 2 números";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtN1;
+        private System.Windows.Forms.Button btnSomar;
+        private System.Windows.Forms.Label lblMensagem;
+        private System.Windows.Forms.TextBox txtN2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
